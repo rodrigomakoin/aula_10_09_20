f48b0786e9152a12f3d1e5ebd343f66efaeef133
rodrigomakoin <48112596+rodrigomakoin@users.noreply.github.com>
2020-09-11 08:21:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 06</title>
+</head>
+<body>
+  <center>
+    <h3>Favor digite dois valores</h3>
+    <br>
+    <br>
+<form method="POST" action="/resultadoPhp.php">
+Valor 1: <input type="number" name="vlr1">
+<br>
+Valor 2: <input type="number" name="vlr2">
+<br>
+Resultado: <input type="submit">
+
+</form>
+
+
+</center>
+</body>
+</html>
